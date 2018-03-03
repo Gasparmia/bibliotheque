f00b7851a4fc63b6e4d864b59ec0a8a5f6f4a16b
Samia <tistonmiamia@gmail.com>
2018-03-03 15:16:55 +0100

    avaivability of user and document

--- routes/emprunter.js
+++ routes/emprunter.js
@@ -13,63 +13,40 @@ router.post('/save', function(req, res, next) {
   };
 
   req.getConnection(function(error, conn) {
-    conn.query('INSERT INTO EMPRUNT SET ?', data, function(err, result) {
-      res.render('emprunt/emprunt.ejs', { result });
-    });
+    conn.query(
+      'select * FROM EMPRUNT WHERE DOCUMENTISBN = ? AND DateRetour IS NULL',
+      [data.DocumentISBN],
+      function(err, result) {
+        console.log(result);
+        if (err) throw err;
+        if (result && result.length > 0) {
+          res.render('emprunt/emprunt.ejs', { err: 'NA' });
+        } else {
+          conn.query(
+            "select * from EMPRUNTEUR WHERE ID= ? AND (`DateFinSanction`>= NOW() OR STATUT = 'bloqué')",
+            [data.EmprunteurId],
+            function(err, result) {
+              console.log(result);
+              if (err) throw err;
+              if (result && result.length > 0) {
+                res.render('emprunt/emprunt.ejs', { err: 'UB' }); //user blocked
+              } else {
+                conn.query('INSERT INTO EMPRUNT SET ?', data, function(
+                  err,
+                  result,
+                ) {
+                  res.render('emprunt/emprunt.ejs', {
+                    err: err,
+                    result: result,
+                  });
+                });
+              }
+            },
+          );
+        }
+      },
+    );
   });
 });
-
-// router.post('/save', function(req, res, next) {
-
-//     conn.query( 'Select ISBN from EMPRUNT WHERE datedEmprunt = ?', [emprunt], function (err, result) {
-//         if (err) throw(err);
-//     });
-// res.render ('emprunt/emprunt.ejs')
-// //envoyer requête pour correction a achref
-
-// //console.log(req.body.isbn);
-//   /*
-// //var data = {
-
-//     NumEmprunt: req.body.NumEmprunt;
-//     DatedEmprunt: req.body.DatedEmprunt;
-//     DateRetour: req.body.DateRetour;
-//     ISBN: req.body.ISBN;
-//     ID: req.body.ID;
-// };*/
-
-//   req.getConnection(function(error, conn) {
-//     /*conn.query('SELECT * FROM EMPRUNTEUR,EMPRUNT WHERE EMPRUNTEUR.ID= EMPRUNT.ID'){
-//     conn.query('SELECT COUNT(*)  FROM emprunter where ID= ? ', [req,ID], function(err, result) {
-//         if ( rowsemprunteur.n <= rows.nbreemprunt){
-//             VAR DATA =
-//             DatedEmprunt: DATE();
-//             ID: req.body.ID;
-//             ISBN: req.body.ISBN;
-//         }
-//         else
-//         conn.query('insert into Emprunt set ?', data, function(err,result) {
-//         if (err) console.error(err);
-//         res.redirect('/emprunter');
-
-//         con.query('update set etat = ? where etat= ?,[emprunts,req.body.isbn], function (err, rows) {
-//         }
-//         })
-// else
-// alert("nombre d'emprunt maximum atteint") ;
-
-//         if (err) throw(err);
-
-//       })};
-//       }
-
-//         conn.query('SELECT ID FROM emprunt WHERE dateretour = UNDEFINED;'),data, function (err,result)
-//       ) {
-
-//       }
-//     });
-//   });*/
-//   });
-// });
 
 module.exports = router;
